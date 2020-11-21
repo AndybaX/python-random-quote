ac5656b28497517101eaac7cf1260b71566182a6
AndybaX <74694914+AndybaX@users.noreply.github.com>
2020-11-20 21:45:54 -0800

    another commit

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,15 @@
+import random
 def primary():
-   print("Keep it logically awesome Dude.")
-   print('Hello')
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  #  print("Keep it logically awesome Dude.")
+  #  print('Hello')
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
+  # print(quotes)
+  #print(quotes[-1])
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
