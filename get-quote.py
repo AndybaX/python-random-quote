494f259d7528075e10a89de168a782d649b64611
Andrew <74694914+AndybaX@users.noreply.github.com>
2021-08-30 17:30:44 -0700

    Another add commit push this from the command line

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,21 @@
-def mymain():
-  print("Keep it logically awesome Dude.")
+import random
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+def mymain():
+  #print("Keep it logically awesome Dude.")
+
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+
+  #last = 13
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
+
 
   #print(quotes)
+  #print(quotes[0]) #First quote
+  #print(quotes[-1]) # Print the last one!
+  print(quotes[rnd])
 
 if __name__== "__main__":
   mymain()
